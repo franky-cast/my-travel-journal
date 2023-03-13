67a9cb65e174061c746b974f5c1faa3ba0b8096d
Francisco Castillo <frankycstl@gmail.com>
2023-03-12 20:41:30 -0700

    data.js update

--- public/assets/data.js
+++ public/assets/data.js
@@ -1,21 +1,38 @@
 export default [
     {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-    },
-    {
         title: "Big Bear Lake",
         location: "California, USA",
         googleMapsUrl: "https://www.google.com/maps?q=big+bear+lake+california&um=1&ie=UTF-8&sa=X&ved=2ahUKEwil2cWJze78AhWerIkEHRBRDiAQ_AUoAXoECAEQAw",
         startDate: "27 Jan, 2021",
         endDate: "30 Jan, 2021",
         description: "Big Bear Lake is a beautiful alpine lake surrounded by mountains, forests and scenic trails in California. It's a popular destination for boating, fishing, hiking, and skiing.",
-        // imageUrl: "https://source.unsplash.com/7bMjJnEaGc8"
         imageUrl:"https://pandareviewz.com/wp-content/uploads/2017/02/big-bear-lake.jpg"
+    },
+    {
+        title: "Santa Barbara",
+        location: "California, USA",
+        googleMapsUrl: "https://www.google.com/maps?q=santa+barbara+california&um=1&ie=UTF-8&sa=X&ved=2ahUKEwi_8ZbT1u_3AhUxmuAKHRiKCcUQ_AUoAXoECAEQAw",
+        startDate: "24 Feb, 2023",
+        endDate: "27 Feb, 2023",
+        description: "Santa Barbara is a picturesque coastal city in California, known for its Mediterranean-style architecture, beaches, and wineries. It's a popular destination for outdoor activities like hiking, surfing, and kayaking.",
+        imageUrl:""
+    },
+    {
+        title: "Grand Canyon",
+        location: "Arizona, USA",
+        googleMapsUrl: "https://www.google.com/maps?q=grand+canyon+national+park&um=1&ie=UTF-8&sa=X&ved=2ahUKEwj6kMv4qu_3AhUJUt8KHV1xAU4Q_AUoAXoECAEQAw",
+        startDate: "24 Mar, 2023",
+        endDate: "27 Mar, 2023",
+        description: "The Grand Canyon is a breathtaking natural wonder located in Arizona. The canyon offers stunning views and opportunities for hiking, camping, and river rafting.",
+        imageUrl:""
+    },
+    {
+        title: "Mexico City",
+        location: "Mexico",
+        googleMapsUrl: "https://www.google.com/maps?q=mexico+city&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiUyJ6Wze78AhUFWK0KHQGKCUcQ_AUoAXoECAEQAw",
+        startDate: "16 Jun, 2023",
+        endDate: "19 Jun, 2023",
+        description: "Mexico City is the vibrant and bustling capital of Mexico, known for its rich history, art, culture, and food. It's home to numerous museums, historic sites, parks, and restaurants serving up delicious Mexican cuisine.",
+        imageUrl:""
     }
 ]
